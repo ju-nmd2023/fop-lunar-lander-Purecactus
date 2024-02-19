3d447a296d2f3da552cd685417345c473dec1292
Purecactus <zaha23ct@student.ju.se>
2024-02-19 19:04:33 +0100

    started doing the stars

--- background.js
+++ background.js
@@ -1,4 +1,18 @@
 // Here I will create a background with animated stars
 
 createCanvas(500, 700);
-background(255);
+
+// I stole this from the lecture in canvas
+let starX = [];
+let starY = [];
+let starAlpha = [];
+
+for (let i = 0, i < 50, i++) {
+    const x = Math.floor(Math.random() * width);
+    const y = Math.floor(Math.random() * height);
+    const alpha = Math.random();
+
+    starX.push(x);
+    starY.push(y);
+    starAlpha.push(alpha);
+}
